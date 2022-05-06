ea0554b7e404fc5355f54b94f3641aa24deb5129
Roy Hsu <roy.hsu@tinyworld.cc>
2022-05-06 12:36:57 +0800

    remove unexpected background color for table view cells since iOS 14.

--- Sources/TinyUIKit/UITableView/UITableViewBridgeCell.swift
+++ Sources/TinyUIKit/UITableView/UITableViewBridgeCell.swift
@@ -10,6 +10,29 @@ import UIKit
 public final class UITableViewBridgeCell<UIViewType: UIView>: UITableViewCell {
   private(set) weak var bridgeView: UIViewType?
 
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setUp()
+  }
+    
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    self.setUp()
+  }
+  
+  private func setUp() {
+    // Starting in iOS 14.0, some system cell configurations will contain
+    // unexpected background color.
+    // We need to explicitly remove it to keep cell background with clear color.
+    backgroundColor = .clear
+    contentView.backgroundColor = .clear
+    if #available(iOS 14.0, *) {
+      var configuration = UIBackgroundConfiguration.listPlainCell()
+      configuration.backgroundColor = .clear
+      backgroundConfiguration = configuration
+    }
+  }
+  
   public override func prepareForReuse() {
     super.prepareForReuse()
     let reusableView = bridgeView as? Reusable
